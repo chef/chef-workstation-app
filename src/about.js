61269e7cfcf0048b730e87fb11bc344ebc5313ac
susanev <susan.ra.evans@gmail.com>
2018-08-29 10:14:59 -0700

    updated height of window

--- src/about.js
+++ src/about.js
@@ -2,7 +2,7 @@ const { BrowserWindow } = require('electron');
 const path = require('path');
 
 const width = process.platform === 'darwin' ? 510 : 530;
-const height = process.platform === 'darwin' ? 325 : 330;
+const height = process.platform === 'darwin' ? 345 : 340;
 
 let aboutDialog = null;
 
