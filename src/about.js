407a1ee6b148ac89527778a84cb9e34fcec56462
Jon Morrow <jmorrow@chef.io>
2018-10-25 20:22:22 -0700

    Enable typescript and move some files around
    - Move main.js into src folder.
    - Moves electron-builder config to its own file.
    - Package assets in app
    - Update paths
    
    Signed-off-by: Jon Morrow <jmorrow@chef.io>

--- src/about.js
+++ src/about.js
@@ -1,26 +1,26 @@
-const { shell } = require('electron');
+const { ipcRenderer, shell } = require('electron');
 const path = require('path');
 const helpers = require('./helpers.js');
 // This is some magic to get the same module as the one loaded in the main process
 // so that our caches are the same.
-const workstation = require('electron').remote.require('./src/chef_workstation.js');
+const workstation = require('electron').remote.require('./chef_workstation.js');
 
 // We open all these links in the users default browser (or what they have setup by default to open HTML).
 // We purposefully decided to open it in the system browser instead of a build in Electron window because
 // we eventually want most of these to be links to documentation on the Chef website. The license should
 // always display from local source but we also want these 3 links to all behave similarly.
 function openLicense() {
-  licensePath = path.join('file://', helpers.getResourcesPath(), 'assets/html/license.html');
+  licensePath = path.join('file://', __dirname, '../assets/html/license.html');
   shell.openExternal(licensePath)
 }
 
 function openReleaseNotes() {
-  notesPath = path.join('file://', helpers.getResourcesPath(), 'assets/html/release_notes.html');
+  notesPath = path.join('file://', __dirname, '../assets/html/release_notes.html');
   shell.openExternal(notesPath)
 }
 
 function openPackageDetails() {
-  detailsPath = path.join('file://', helpers.getResourcesPath(), 'assets/html/package_details.html');
+  detailsPath = path.join('file://', __dirname, '../assets/html/package_details.html');
   shell.openExternal(detailsPath)
 }
 
@@ -36,8 +36,7 @@ function getSwitchToChannel() {
 function toggleUpdatesChannel() {
   workstation.setUpdateChannel(getSwitchToChannel());
   updateDialog();
-  const app = require('electron').remote.app;
-  app.emit('do-update-check', true, false);
+  ipcRenderer.send('do-update-check', true, false);
 }
 
 function updateDialog() {
