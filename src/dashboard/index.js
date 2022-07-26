7d3a6876bf9de695ee61c68d87ef1b0678e0eaeb
nikhil2611 <nikhilgupta2102@gmail.com>
2022-07-26 10:36:05 +0530

    listing of cookboks and recipes
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -2,41 +2,67 @@ const path = require("path");
 const fs = require("fs");
 const helpers = require("../helpers/helpers.js");
 
+let html = "<ul>";
+// Statup th page
 function render() {
-  document.getElementById("repos").addEventListener("click", () => {
-    const repository = path.join("file://", __dirname, "repos.html");
-    let os = require("os");
-    fetch(repository)
-      .then((response) => response.text())
-      .then((text) => {
-        document.querySelector("#content").innerHTML = text;
-        const fileObj = helpers.readRepoPath();
-        try {
-          let filesNames = new Map();
-          for (i = 0; i < fileObj.length; i++) {
-            const fileName = fileObj[i].filepath.replace(/^.*[\\\/]/, "");
-            fs.existsSync(fileObj[i].filepath)
-              ? filesNames.set(fileName, true)
-              : filesNames.set(fileName, false);
-          }
+  try {
+    const directories = [];
+    const fileObj = helpers.readRepoPath();
+    for (i = 0; i < fileObj.length; i++) {
+      // get all the directories and generate the tree
+      directories.push(createDirTree(fileObj[i].filepath));
+    }
+    // create the Bootstrap collapse
+    createCollapse(directories);
+    html += "</ul>";
+    document.getElementById("tree").innerHTML = html;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-          var ul = document.getElementById("repos-folder");
-          filesNames.forEach((fileStatus, key) => {
-            console.log(fileStatus, key);
-            var li = document.createElement("li");
-            document.body.appendChild(li);
-            li.innerHTML = fileStatus
-              ? key + "&nbsp;&nbsp;Active"
-              : key + "&nbsp;&nbsp;Inactive";
+function createDirTree(filename) {
+  const stats = fs.lstatSync(filename),
+    info = {
+      path: filename,
+      name: path.basename(filename),
+    };
 
-            ul.appendChild(li);
-            //   }
-          });
-        } catch (err) {
-          console.error(err);
-        }
-      });
-  });
+  if (stats.isDirectory()) {
+    info.type = "folder";
+
+    info.children = fs.readdirSync(filename).reduce(function (obj, child) {
+      if (child[0] !== ".") {
+        obj.push(createDirTree(filename + "/" + child));
+      }
+      return obj;
+    }, []);
+  } else {
+    // Assuming it's a file. In real life it could be a symlink or
+    // something else!
+    info.type = "file";
+  }
+
+  return info;
+}
+
+function createCollapse(data, name) {
+  for (let i = 0; i < data.length; i++) {
+    const current = data[i];
+    if ("children" in current) {
+      const random = Math.floor(Math.random() * 10000) + 1;
+      html += `<li><a href=#u${random} data-bs-toggle="collapse" aria-expanded="true"> 
+        ${current.name} 
+        </a>`;
+      html += `<ul id=u${random}>`;
+      createCollapse(current.children, current.name);
+    } else {
+      html += "<li>" + current.name + "</li>";
+    }
+  }
+  html += "</li></ul>";
+  return data.name;
 }
 
 module.exports = { render };
+
