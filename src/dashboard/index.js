8038d571ad0b252c4108ea95b72ac52cd5dd38d2
Pranay Singh <i5singh.pranay@gmail.com>
2022-09-06 11:07:14 +0530

    adding ui in table without any api integration
    Signed-off-by: Pranay Singh <i5singh.pranay@gmail.com>

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const helpers = require("../helpers/helpers.js");
+const {setAttribute} = require("jsdom/lib/jsdom/living/attributes");
 
 function render() {
   document.getElementById("repos").addEventListener("click", () => {
@@ -47,28 +48,36 @@ function render() {
                 document.querySelector("#content").innerHTML = text;
                 var cookbook_activity = document.createElement('div');
                 cookbook_activity.setAttribute("id", "cookbook-activity");
+
+                var container_cookbook_details = document.createElement('div');
+                container_cookbook_details.setAttribute("class", "container");
+
                 var cookbook_details = document.createElement('div');
                 cookbook_details.setAttribute("class", "cookbook-details");
                 // todo add left padding
                 var breadcrumb_nav = document.createElement('nav');
                 breadcrumb_nav.setAttribute('aria-label', 'breadcrumb')
 
+                var clear_fix =  document.createElement('div');
+                // clearfix to add space
+
                 var bc_ol = document.createElement('ol');
                 bc_ol.setAttribute('class', 'breadcrumb')
 
-                var breadcrumb_arr = ['Activity', 'Cookbooks', 'Recent Updates']
+                var breadcrumb_arr = ['Activity', 'Cookbooks', 'Recent Updates']  // todo - Api call will fetch this data
                 for (var i = 0; i < breadcrumb_arr.length; i++) {
                     var li = document.createElement('li');
+                    // todo write a logic tp get the active class (get the active e.g 'cookbook index number and macth with breadcrump cookbook')
                     li.setAttribute('class', 'breadcrumb-item')
                     var anchor_tag = document.createElement('a');
                     anchor_tag.setAttribute('href', "#")
                     var text = document.createTextNode(breadcrumb_arr[i]);
                     anchor_tag.appendChild(text)
                     li.appendChild(anchor_tag);
+                    bc_ol.appendChild(li)
                 }
 
-                breadcrumb_nav.appendChild(bc_ol);
-                cookbook_details.appendChild(breadcrumb_nav);
+
 
                 // todo  this data need to come dynamically aswell
                 var h3_tag = document.createElement('h3');
@@ -76,14 +85,64 @@ function render() {
                 var text1 = document.createTextNode(breadcrumb_arr[1]);
                 var text2 = document.createTextNode(breadcrumb_arr[2]);
 
+                breadcrumb_nav.appendChild(bc_ol);
+                cookbook_details.appendChild(breadcrumb_nav);
+
                 h3_tag.appendChild(text1)
                 h5_tag.appendChild(text2)
                 cookbook_details.appendChild(h3_tag);
                 cookbook_details.appendChild(h5_tag);
 
+                container_cookbook_details.appendChild(cookbook_details)
+                cookbook_activity.appendChild(container_cookbook_details)
 
 
+                // make search  append it inside container row and md-12 along with responsive table
+                var activity_folder = document.createElement('ul');
+                activity_folder.setAttribute('id', 'activity-folder')
 
+                var container_class = document.createElement('div');
+                container_class.setAttribute('class', 'container')
+
+                var row_class = document.createElement('div');
+                row_class.setAttribute('class', 'row')
+
+                var md_12_class = document.createElement('div');
+                md_12_class.setAttribute('class', 'col-md-12')
+
+                 // search bar
+                var search_bar_block = document.createElement('div');
+                search_bar_block.setAttribute('class', 'search-bar-filter-bar-block')
+
+                var custom_search_input = document.createElement('div');
+                custom_search_input.setAttribute('id', 'custom-search-input')
+
+                var input_grp =  document.createElement('div');
+                input_grp.setAttribute('class', 'input-group col-md-12')
+
+                var input_text =  document.createElement('input');
+                input_text.setAttribute('class', 'search-query form-control')
+                input_text.setAttribute('type', 'text')
+                input_text.setAttribute('placeholder', 'search cookbooks& recipes')
+
+
+                var search_button = document.createElement('button');
+                search_button.setAttribute('class', 'btn btn-danger')
+                search_button.setAttribute('type', 'button')
+                var text = document.createTextNode("search");
+                search_button.appendChild(text)
+                input_grp.appendChild(input_text)
+                input_grp.appendChild(search_button)
+
+                custom_search_input.appendChild(input_grp)
+                search_bar_block.appendChild(custom_search_input)
+                md_12_class.appendChild(search_bar_block)
+                row_class.appendChild(md_12_class)
+                container_class.appendChild(row_class)
+                activity_folder.appendChild(container_class)
+                activity_folder.appendChild(clear_fix)
+
+                // tables creation
                 var div_table_responsive = document.createElement('div'); // div to encapsulate table
                 div_table_responsive.setAttribute("class", "table-responsive");
 
@@ -94,48 +153,24 @@ function render() {
                 var thead =  document.createElement('thead');
                 var tboady = document.createElement('tbody');
 
+
                 // var tr = document.createElement('tr');
                 var arrheader = [' ', 'Cookbooks', 'Repository', 'Recipes', 'Policyfile', 'Created at', 'Action'];
 
 
                 // this has to come dynamically from reading directory structure (This should come from api)
-                var array = [{
-                    check_list: '',
-                    cookbooks: 'Zen_apache',
-                    repository: 'my_repo',
-                    recipes: '20(show >>)',
-                    policyfile: 'config.rb',
-                    created_at: '0 am, 27/3/22',
-                    action: 'Upload'
-                },
-                    {
-                        check_list: '',
-                        cookbooks: 'Zen_apache',
-                        repository: 'my_repo',
-                        recipes: '20(show >>)',
-                        policyfile: 'config.rb',
-                        created_at: '0 am, 27/3/22',
-                        action: 'Upload'
-                    },
-                    {
-                        check_list: '',
-                        cookbooks: 'Zen_apache',
-                        repository: 'my_repo',
-                        recipes: '20(show >>)',
-                        policyfile: 'config.rb',
-                        created_at: '0 am, 27/3/22',
-                        action: 'Upload'
-                    },
-                    {
-                        check_list: '',
-                        cookbooks: 'Zen_apache',
-                        repository: 'my_repo',
-                        recipes: '20(show >>)',
-                        policyfile: 'config.rb',
-                        created_at: '0 am, 27/3/22',
-                        action: 'Upload'
-                    }
-                ];
+               //  todo - Api will fetch this data
+               var ele = {
+                   check_list: '',
+                   cookbooks: 'Zen_apache',
+                   repository: 'my_repo',
+                   recipes: '20(show >>)',
+                   policyfile: 'config.rb',
+                   created_at: '0 am, 27/3/22',
+                   action: 'Upload'
+               }
+
+               var array =  Array(50).fill(ele)
 
                 for (var j = 0; j < arrheader.length; j++) {
                     var th = document.createElement('th'); //column
@@ -151,7 +186,7 @@ function render() {
                     var td1 = document.createElement('td');
 
                     var input_1 =  document.createElement('input'); //input form
-                    input_1.setAttribute("type", "checkbox");
+                    input_1.setAttribute("type", "checkbox"); // this will be set based on input
                     input_1.setAttribute("class", "checkthis");
 
                     // check if it is is checked
@@ -161,13 +196,24 @@ function render() {
                     var td4 = document.createElement('td');
                     var td5 = document.createElement('td');
                     var td6 = document.createElement('td');
+                    var td7 = document.createElement('td');
 
                     // var text1 = document.createTextNode(array[i].check_list); // this will give is checked value from api
-                    var text2 = document.createTextNode(array[i].cookbooks);
-                    var text3 = document.createTextNode(array[i].recipes);
-                    var text4 = document.createTextNode(array[i].policyfile);
-                    var text5 = document.createTextNode(array[i].created_at);
-                    var text6 = document.createTextNode(array[i].action);
+                    var text2 = document.createTextNode(array[i].repository);
+                    var text3 = document.createTextNode(array[i].cookbooks);
+                    var text4 = document.createTextNode(array[i].recipes);
+                    var text5 = document.createTextNode(array[i].policyfile);
+                    var text6 = document.createTextNode(array[i].created_at);
+                    var btn = document.createElement('button');
+                    btn.setAttribute("class", "btn btn-success");
+
+                    var btn_span = document.createElement('button');
+                    btn_span.setAttribute("class", "btn btn-primary");
+
+                    var span_text = document.createTextNode('remove');
+
+                    var text7 = document.createTextNode(array[i].action);
+
 
                     td1.appendChild(input_1);
                     td2.appendChild(text2);
@@ -175,6 +221,10 @@ function render() {
                     td4.appendChild(text4);
                     td5.appendChild(text5);
                     td6.appendChild(text6);
+                    td7.appendChild(btn);
+                    btn.appendChild(text7);
+                    td7.appendChild(btn_span);
+                    btn_span.appendChild(span_text);
 
                     tr.appendChild(td1);
                     tr.appendChild(td2);
@@ -182,22 +232,35 @@ function render() {
                     tr.appendChild(td4);
                     tr.appendChild(td5);
                     tr.appendChild(td6);
+                    tr.appendChild(td7);
 
 
                     tboady.appendChild(tr);
                     table.appendChild(tboady);
+
                 }
 
-                var clear_fix =  document.createElement('div');
+
+
                 clear_fix.setAttribute('class', 'clearfix')
 
                 div_table_responsive.appendChild(table);
+                div_table_responsive.setAttribute('style', ' display: block; height: 600px; overflow-y: scroll;')
                 div_table_responsive.appendChild(clear_fix);
-                cookbook_activity.appendChild(div_table_responsive);
+                activity_folder.appendChild(div_table_responsive);
+                cookbook_activity.appendChild(activity_folder);
                 document.querySelector("#content").appendChild(cookbook_activity);
+
+            //    adding infinite scroll
+                table.addEventListener("scroll", () => {
+
+                });
+            //     infinite scroll code ends
+
 
             });
     });
+
 }
 
 module.exports = { render };
