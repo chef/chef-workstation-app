aba78a6000cd6ac56bf131ba7309b3d568d718b9
nikhil2611 <nikhilgupta2102@gmail.com>
2022-11-25 14:35:43 +0530

    handled the auth error
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -3,13 +3,20 @@ const fs = require("fs");
 const helpers = require("../helpers/helpers.js");
 const { setAttribute } = require("jsdom/lib/jsdom/living/attributes");
 const axios = require("axios");
-const repoKey = "/opt/chef-workstation/service.txt";
+let repoKey = "";
 let authToken = "";
+
+if(process.platform === 'darwin'){
+  repoKey = "/opt/chef-workstation/service.txt";
+} else {
+  repoKey = "c:\\opscode\\chef-workstation\\service.txt";
+}
+
 
 function readRepoKey() {
     const fileData = fs.readFileSync(repoKey).toString("utf8");
     var accessKey = {
-        access_key: fileData.replace(/\n/g, ""),
+        access_key: fileData.replace(/(\r\n|\n|\r)/gm, ""),
     };
     generateToken(accessKey);
 }
@@ -31,7 +38,7 @@ axios.interceptors.request.use(
     (request) => {
         if (
             request.url.includes("list_repositories") ||
-            request.url.includes("cookbooks")
+            request.url.includes("cookbooks") || request.url.includes("cookbook") || request.url.includes("link_repository")
         ) {
             request.headers["Authorization"] = authToken;
         }
@@ -48,19 +55,11 @@ axios.interceptors.response.use(
     },
     (error) => {
         if (error.response.status === 401) {
-            console.log("testing---");
             readRepoKey();
         }
         return error;
     }
 );
-
-// axios.interceptors.response.use(undefined, function axiosRetryInterceptor(err) {
-//   if (err.response.status === 401 || err.response.data.message === '401 Unauthorized') {
-//     readRepoKey();
-//   }
-//   // return Promise.reject(err);
-// })
 
 function render() {
     readRepoKey();
@@ -89,7 +88,6 @@ function render() {
                 tablecontents += "<tr>";
                 const rows = Object.values(repositories[i]);
                 tablecontents += "<td>" + rows[2] + "</td>";
-                // tablecontents += "<td>" + rows[2] + "</td>";
 
                 tablecontents += "</tr>";
             }
@@ -99,12 +97,12 @@ function render() {
         }
     });
 
-    let axiosConfig = {
-        headers: {
-            Authorization:
-                "eyJhbGciOiJIUzI1NiJ9.eyJhY2Nlc3Nfa2V5IjoiNjU2MGYxOThjNTU1NTUwYWU3Y2UyYWZlNDdjZTlmZDEiLCJleHAiOjE2Njk2MjYwMzd9.dP3lZGcBKwC4qJFWC2RrugOqk1wVrHa9w80aT-eAFmM",
-        },
-    };
+    // let axiosConfig = {
+    //     headers: {
+    //         Authorization:
+    //             "eyJhbGciOiJIUzI1NiJ9.eyJhY2Nlc3Nfa2V5IjoiNjU2MGYxOThjNTU1NTUwYWU3Y2UyYWZlNDdjZTlmZDEiLCJleHAiOjE2Njk2MjYwMzd9.dP3lZGcBKwC4qJFWC2RrugOqk1wVrHa9w80aT-eAFmM",
+    //     },
+    // };
 
     document.getElementById("cookbooks").addEventListener("click", async () => {
         document.getElementById("chef-cookbook-content").innerHTML = "";
@@ -132,10 +130,6 @@ function render() {
         const {data: {cookbooks}} = await axios({
             method: "get",
             url: "http://127.0.0.1:7050/api/v1/repositories/cookbooks",
-            // headers: {
-            //   Authorization:
-            //     "eyJhbGciOiJIUzI1NiJ9.eyJhY2Nlc3Nfa2V5IjoiNjU2MGYxOThjNTU1NTUwYWU3Y2UyYWZlNDdjZTlmZDEiLCJleHAiOjE2Njk2MjYwMzd9.dP3lZGcBKwC4qJFWC2RrugOqk1wVrHa9w80aT-eAFmM",
-            // },
         });
         let tablecontents = "";
         for (var i = 0; i < cookbooks.length; i++) {
@@ -158,8 +152,6 @@ function render() {
         const upalodCookBooks = function () {
             const attribute = this.getAttribute("data-cookbook");
             const path = this.getAttribute("path");
-            console.log("attr--", attribute); //
-            console.log("path--", path.substring(0, path.lastIndexOf("/"))); //
             var postData = {
                 cookbook_name: attribute,
                 cookbook_path: path.substring(0, path.lastIndexOf("/")),
@@ -167,8 +159,8 @@ function render() {
             axios
                 .post(
                     "http://127.0.0.1:7050/api/v1/cookbook",
-                    postData,
-                    axiosConfig
+                    postData
+                    // axiosConfig
                 )
                 .then(function (response) {
                     console.log(response);
@@ -182,235 +174,5 @@ function render() {
             elements[i].addEventListener("click", upalodCookBooks, false);
         }
     });
-
-    // document.getElementById("overview").addEventListener("click", () => {
-    //   document.querySelector("#overview-content").style.display = "block";
-    //   document.getElementById("chef-cookbook-content").innerHTML = "";
-    //   document.getElementById("chef-repo-content").style.display = "none";
-    //   document.getElementById("chef-repo-list").innerHTML = "";
-
-    // });
-
-    // document.getElementById("activity").addEventListener("click", () => {
-    //   const repository = path.join("file://", __dirname, "activity.html");
-    //   let os = require("os");
-    //   fetch(repository)
-    //     .then((response) => response.text())
-    //     .then((text) => {
-    //       document.querySelector("#content").innerHTML = text;
-    //       var cookbook_activity = document.createElement("div");
-    //       cookbook_activity.setAttribute("id", "cookbook-activity");
-
-    //       var container_cookbook_details = document.createElement("div");
-    //       container_cookbook_details.setAttribute("class", "container");
-
-    //       var cookbook_details = document.createElement("div");
-    //       cookbook_details.setAttribute("class", "cookbook-details");
-    //       // todo add left padding
-    //       var breadcrumb_nav = document.createElement("nav");
-    //       breadcrumb_nav.setAttribute("aria-label", "breadcrumb");
-
-    //       var clear_fix = document.createElement("div");
-    //       // clearfix to add space
-
-    //       var bc_ol = document.createElement("ol");
-    //       bc_ol.setAttribute("class", "breadcrumb");
-
-    //       var breadcrumb_arr = ["Activity", "Cookbooks", "Recent Updates"]; // todo - Api call will fetch this data
-    //       for (var i = 0; i < breadcrumb_arr.length; i++) {
-    //         var li = document.createElement("li");
-    //         // todo write a logic tp get the active class (get the active e.g 'cookbook index number and macth with breadcrump cookbook')
-    //         li.setAttribute("class", "breadcrumb-item");
-    //         var anchor_tag = document.createElement("a");
-    //         anchor_tag.setAttribute("href", "#");
-    //         var text = document.createTextNode(breadcrumb_arr[i]);
-    //         anchor_tag.appendChild(text);
-    //         li.appendChild(anchor_tag);
-    //         bc_ol.appendChild(li);
-    //       }
-
-    //       // todo  this data need to come dynamically aswell
-    //       var h3_tag = document.createElement("h3");
-    //       var h5_tag = document.createElement("h5");
-    //       var text1 = document.createTextNode(breadcrumb_arr[1]);
-    //       var text2 = document.createTextNode(breadcrumb_arr[2]);
-    //       h5_tag.setAttribute("style", "text-decoration: underline");
-
-    //       breadcrumb_nav.appendChild(bc_ol);
-    //       cookbook_details.appendChild(breadcrumb_nav);
-
-    //       h3_tag.appendChild(text1);
-    //       h5_tag.appendChild(text2);
-    //       cookbook_details.appendChild(h3_tag);
-    //       cookbook_details.appendChild(h5_tag);
-
-    //       container_cookbook_details.appendChild(cookbook_details);
-    //       cookbook_activity.appendChild(container_cookbook_details);
-
-    //       // make search  append it inside container row and md-12 along with responsive table
-    //       var activity_folder = document.createElement("ul");
-    //       activity_folder.setAttribute("id", "activity-folder");
-
-    //       var container_class = document.createElement("div");
-    //       container_class.setAttribute("class", "container");
-
-    //       var row_class = document.createElement("div");
-    //       row_class.setAttribute("class", "row");
-
-    //       var md_12_class = document.createElement("div");
-    //       md_12_class.setAttribute("class", "col-md-12");
-
-    //       // search bar
-    //       var search_bar_block = document.createElement("div");
-    //       search_bar_block.setAttribute("class", "search-bar-filter-bar-block");
-
-    //       var custom_search_input = document.createElement("div");
-    //       custom_search_input.setAttribute("id", "custom-search-input");
-
-    //       var input_grp = document.createElement("div");
-    //       input_grp.setAttribute("class", "input-group col-md-12");
-
-    //       var input_text = document.createElement("input");
-    //       input_text.setAttribute("class", "search-query form-control");
-    //       input_text.setAttribute("type", "text");
-    //       input_text.setAttribute("placeholder", "search cookbooks& recipes");
-
-    //       var search_button = document.createElement("button");
-    //       search_button.setAttribute("class", "btn btn-danger");
-    //       search_button.setAttribute("type", "button");
-    //       var text = document.createTextNode("search");
-    //       search_button.appendChild(text);
-    //       input_grp.appendChild(input_text);
-    //       input_grp.appendChild(search_button);
-
-    //       custom_search_input.appendChild(input_grp);
-    //       search_bar_block.appendChild(custom_search_input);
-    //       md_12_class.appendChild(search_bar_block);
-    //       row_class.appendChild(md_12_class);
-    //       container_class.appendChild(row_class);
-    //       activity_folder.appendChild(container_class);
-    //       activity_folder.appendChild(clear_fix);
-
-    //       // tables creation
-    //       var div_table_responsive = document.createElement("div"); // div to encapsulate table
-    //       div_table_responsive.setAttribute("class", "table-responsive");
-
-    //       var table = document.createElement("table");
-    //       table.setAttribute("id", "mytable");
-    //       table.setAttribute("class", "table table-bordred table-striped");
-
-    //       var thead = document.createElement("thead");
-    //       var tboady = document.createElement("tbody");
-
-    //       var arrheader = [
-    //         " ",
-    //         "Cookbooks",
-    //         "Repository",
-    //         "Recipes",
-    //         "Policyfile",
-    //         "Action",
-    //       ];
-
-    //       for (var j = 0; j < arrheader.length; j++) {
-    //         var th = document.createElement("th"); //column
-    //         var text = document.createTextNode(arrheader[j]); //cell
-    //         th.appendChild(text);
-    //         thead.appendChild(th);
-    //       }
-
-    //       table.appendChild(thead);
-
-    //       //     Note -- API CALL TO GET LIST OF ALL REPOSITORIES
-    //       axios.defaults.headers.post["Content-Type"] = "application/json";
-    //       axios
-    //         .get("http://localhost:3001/api/v1/repositories/cookbooks", {
-    //           /* here you can pass any parameters you want */
-    //         })
-    //         .then((response) => {
-    //           console.log(response);
-    //           if (response.status == 200) {
-    //             // todo - handle 422 and fail condition
-    //             array = response.data.cookbooks;
-    //             for (var i = 0; i < array.length; i++) {
-    //               var tr = document.createElement("tr");
-
-    //               var td1 = document.createElement("td");
-
-    //               var input_1 = document.createElement("input"); //input form
-    //               input_1.setAttribute("type", "checkbox"); // this will be set based on input
-    //               input_1.setAttribute("class", "checkthis");
-
-    //               // check if it is is checked
-
-    //               var td2 = document.createElement("td");
-    //               var td3 = document.createElement("td");
-    //               var td4 = document.createElement("td");
-    //               var td5 = document.createElement("td");
-    //               var td7 = document.createElement("td");
-
-    //               // var text1 = document.createTextNode(array[i].check_list); // this will give is checked value from api
-    //               var text2 = document.createTextNode(array[i].cookbook_name);
-    //               var text3 = document.createTextNode(array[i].repository);
-    //               var text4 = document.createTextNode(array[i].recipe_count);
-    //               var text5 = document.createTextNode(array[i].policyfile);
-
-    //               var btn = document.createElement("button");
-    //               btn.setAttribute("class", "btn btn-success");
-
-    //               // var btn_span = document.createElement('button');
-    //               // btn_span.setAttribute("class", "btn btn-primary");
-    //               //
-    //               // var span_text = document.createTextNode('remove'); // Todo --> commenting unlink for now(basically this is unlink)
-
-    //               // td7.appendChild(btn_span);
-    //               // btn_span.appendChild(span_text);
-
-    //               var text7 = document.createTextNode(
-    //                 array[i].actions_available[0]
-    //               ); // Todo -  correct once actions are decided, make it generic
-    //               // right now just choosing one upload
-
-    //               td1.appendChild(input_1);
-    //               td2.appendChild(text2);
-    //               td3.appendChild(text3);
-    //               td4.appendChild(text4);
-    //               td5.appendChild(text5);
-    //               td7.appendChild(btn);
-    //               btn.appendChild(text7);
-
-    //               tr.appendChild(td1);
-    //               tr.appendChild(td2);
-    //               tr.appendChild(td3);
-    //               tr.appendChild(td4);
-    //               tr.appendChild(td5);
-    //               tr.appendChild(td7);
-
-    //               tboady.appendChild(tr);
-    //             }
-    //             table.appendChild(tboady);
-    //           }
-
-    //           clear_fix.setAttribute("class", "clearfix");
-
-    //           div_table_responsive.appendChild(table);
-    //           div_table_responsive.setAttribute(
-    //             "style",
-    //             " display: block; height: 600px; overflow-y: scroll;"
-    //           );
-    //           div_table_responsive.appendChild(clear_fix);
-    //           activity_folder.appendChild(div_table_responsive);
-    //           cookbook_activity.appendChild(activity_folder);
-    //           document.querySelector("#content").appendChild(cookbook_activity);
-
-    //           //    adding infinite scroll
-    //           table.addEventListener("scroll", () => {});
-    //           //     infinite scroll code ends
-    //         }) // ending axios call
-    //         .catch((error) => {
-    //           console.error(error);
-    //           return error;
-    //         });
-    //     });
-    // });
 }
 module.exports = { render };
